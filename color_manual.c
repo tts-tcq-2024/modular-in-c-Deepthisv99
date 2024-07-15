a517054a217edf71e6e8c429604d6161cde6ed25
Deepthisv99 <deepthivenki1@gmail.com>
2024-07-15 14:33:36 +0530

    Create color_manual.c

---
+++ color_manual.c
@@ -0,0 +1,12 @@
+#include "color.h"
+#include <stdio.h>
+
+void PrintColorCodingManual() {
+    printf("Pair Number\tMajor Color\tMinor Color\n");
+    for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; ++i) {
+        ColorPair colorPair = GetColorFromPairNumber(i);
+        char colorPairNames[16];
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("%d\t\t%s\n", i, colorPairNames);
+    }
+}
