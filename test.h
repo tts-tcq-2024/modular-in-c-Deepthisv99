06383d80efdcac8ad18503d802380ba35377e2f1
Deepthisv99 <deepthivenki1@gmail.com>
2024-07-15 14:34:00 +0530

    Create test.h

---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef TEST_H
+#define TEST_H
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
+
+#endif
